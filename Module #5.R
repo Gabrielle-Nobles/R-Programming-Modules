6df008f5bb2d835d4456acdc730567f620b8a4a5
Gabrielle Nobles <97853225+Gabrielle-Nobles@users.noreply.github.com>
2022-02-13 18:46:45 -0500

    Add files via upload

---
+++ Module #5.R
@@ -0,0 +1,29 @@
+#Create the matrices
+A <- matrix(1:100, nrow = 10)
+B <- matrix(1:100, nrow = 10)
+
+#transpose A and B 
+t(A)
+t(B)
+
+#create two vectors (a and b)
+a <- c(1:2)
+b <- c(1:4)
+
+#multiply matrices by vectors 
+C = a*A
+D = b*B
+
+#re-assign the vectors a and b to equal the number of rows of the column for the corresponding matrix
+a =c(1:10)
+b =c(1:100)
+
+#multiply the matrixx by matrix 
+A %*%a
+B&8&b
+
+#inverse a matrix 
+S = matrix(2:5, nrow = 2)
+
+#check det 
+det(S)
