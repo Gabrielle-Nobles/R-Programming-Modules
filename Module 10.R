d50ec42f3f77b68918f21bcf1193ca92035eaf13
Gabrielle Nobles <97853225+Gabrielle-Nobles@users.noreply.github.com>
2022-03-24 21:02:20 -0400

    Create Module 10.R

---
+++ Module 10.R
@@ -0,0 +1,11 @@
+Package: Gabrielle Nobles
+Type: Package
+Title: transfigure
+Version: 0.1.0
+Authors@R: "Gabrielle Nobles <gabriellenobles@usf.edu> [aut, cre]"
+Description: This R package will preform operations that converts the gene expression matrices 
+utlized by bioinformation to make it easier to use for downstream scRNA-seq analysis.
+Depends: R (>= 3.4.4)
+License: CC0
+Encoding: UTF-8
+LazyData: true
